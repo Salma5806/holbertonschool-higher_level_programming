36227770a89c6be41cb5f1a6880cbc65ee5c0532
Salma5806 <5806@holbertonstudents.com>
2023-02-02 20:36:19 +0100

    classs square with size

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square():
+    """Represent a square."""
+
+    def __init__(self, size = None):
+        """Initialize a new Square.
+
+        Args:
+            size (int): the size of the new square.
+        """
+        self.size = None
