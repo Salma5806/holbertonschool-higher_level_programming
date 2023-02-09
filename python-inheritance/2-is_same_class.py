8490bb3b985bcaac15fc04fcd90383ce6543dfb3
Salma5806 <5806@holbertonstudents.com>
2023-02-09 20:00:46 +0100

    same class or no

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" function"""
+def is_same_class(obj, a_class):
+   """
+   print true or false if the object is
+   exactly an instance of the class
+
+   """
+   return (type(obj) is (a_class))
