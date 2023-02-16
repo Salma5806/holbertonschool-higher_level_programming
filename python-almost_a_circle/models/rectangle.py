0831cc63a38d0f21525aabf9d9993a404f821e05
Salma5806 <5806@holbertonstudents.com>
2023-02-16 23:35:27 +0100

    class retangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,41 +1,16 @@
 #!/usr/bin/python3
-"""models Rectangle classess"""
-
+"""Module Rectangle"""
+from .base import Base
 
 
 class Rectangle(Base):
-     """class Rectangle with
-     Args :
-          __width : width of rectangle
-          __height : height of rectangle
-          __x : number
-          __y : number
-     """
-
-     def __init__(self, width, height, x=0, y=0, id=None):
-         super().__init__(id)
-         self.width = width
-         self.height = height
-         self.x = x
-         self.y = y
-
-    @property
-    def width(self):
-        """get width"""
-        return self.__width
-
-    @property
-    def height(self):
-        """get height"""
-        return self.__height
-
-    @property
-    def x(self):
-        """Get x"""
-        return self.__x
-
-    @property
-    def y(self):
-        """Get y"""
-        return self.__y
-
+    """
+    Defines class rectangle
+    """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize a new Rectangle"""
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
