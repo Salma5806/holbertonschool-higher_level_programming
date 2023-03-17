b93ef5914ce56fa377c6758156e98720cf6c151e
Salma5806 <5806@holbertonstudents.com>
2023-03-17 20:03:33 +0100

    cities

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,7 @@
+-- create the database and the table
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
+     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+     state_id INT NOT NULL ,
+     FOREIGN KEY(state_id) REFERENCES states(id),
+     name VARCHAR(256) NOT NULL);
