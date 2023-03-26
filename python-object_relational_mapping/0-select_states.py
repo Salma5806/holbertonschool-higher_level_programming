6569dc86ef8cfe43ac3e366aaa1b4780f4b5e900
Salma5806 <5806@holbertonstudents.com>
2023-03-26 22:49:57 +0100

    state

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,17 +1,29 @@
 #!/usr/bin/python3
-""" Script that lists all states from the database hbtn_0e_0_usa
 """
-if __name__ == "__main__":
-    import sys
-    import MySQLdb
+Module list state
+"""
+import sys
+import MySQLdb
 
-    serv = MySQLdb.connect(host="localhost",  port=3306,
-                           user=sys.argv[1], password=sys.argv[2],
-                           database=sys.argv[3])
-    c = serv.cursor()
-    c.execute("SELECT * FROM states ORDER BY id ASC")
-    rows = c.fetchall()
-    for row in rows:
-        print(row)
-    c.close()
-    serv.close()
+
+def main():
+    conn = MySQLdb.connect(
+                        host="localhost",
+                        port=3306,
+                        user=sys.argv[1],
+                        passwd=sys.argv[2],
+                        db=sys.argv[3],
+                        charset="utf8"
+                            )
+    cur = conn.cursor()
+    query = "SELECT id,name FROM states ORDER by id ASC"
+    cur.execute(query)
+    row = cur.fetchall()
+    for r in row:
+        print(r)
+    cur.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
